1b4839f9f573377bb071a32edd32cbd561f93a7d
Vladan T <Vladan T@vladant>
2017-04-19 17:52:16 +0200

    AVs project StringCalc

---
+++ src/string2calc/String2Calc.java
@@ -0,0 +1,26 @@
+package string2calc;
+public class String2Calc {
+
+   public static void main(String... args) {
+    String a = "aaabcde";
+    int currentLength = 1;
+
+    StringBuilder result = new StringBuilder(); // empty string
+    for (int i = 1; i < a.length(); i++) {
+        if (a.charAt(i) == a.charAt(i - 1)) {
+            currentLength++;
+        } else {
+            System.out.println(currentLength + "" + a.charAt(i - 1));
+            result.append(a.charAt(i - 1))
+                .append(currentLength > 1 ? currentLength : "");
+            currentLength = 1; // reset the length
+        }
+    }
+
+    // print last one
+    System.out.println(currentLength + "" + a.charAt(a.length() - 1));
+    result.append(a.charAt(a.length() - 1))
+        .append(currentLength > 1 ? currentLength : "");
+    System.out.println("result = " + result.toString());
+}
+}
